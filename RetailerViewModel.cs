2ce906dda28eb7fad521924d235b6ef2c649a1f3
Sarthak Lav <sarthakchaudhary52@gmail.com>
2019-10-29 20:15:37 +0530

    Add files via upload

---
+++ RetailerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace GreatOutdoors.Mvc.Models
+{
+    public class RetailerViewModel
+    {
+        public System.Guid RetailerID { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Retailer Name can't be empty")]
+        [RegularExpression("^[A-Za-z]*$", ErrorMessage = "Retailer Name should contain alphabets only")]
+        [Display(Name = "Name")]
+        public string RetailerName { get; set; }
+        [Required(ErrorMessage = "Retailer Mobile can't be empty")]
+        [RegularExpression("^([9]{1})([234789]{1})([0-9]{8})$", ErrorMessage = "Retailer Mobile should contain number only")]
+        [Display(Name = "Mobile")]
+        public string RetailerMobile { get; set; }
+        [Required(ErrorMessage = "Email can't be empty")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email should be in proper format")]
+        // Add regex  AllowEmptyStrings = false,
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Pasword can't be empty")]
+        // [RegularExpression(@"(?=^.{6,}$)(?=.*\d)(?=.*[a-zA-Z])", ErrorMessage = "Password should contain atleast 6 digits and must include one upper case, one lower case and one number")]
+        [Display(Name = "Password")]
+        public string RetailerPassword { get; set; }
+        public Nullable<decimal> RetailerDiscountPercentage { get; set; }
+        public System.DateTime CreationDateTime { get; set; }
+        public Nullable<System.DateTime> ModifiedDateTime { get; set; }
+        public bool IsAdded { get; set; }
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Select a gender")]
+        public string Gender { get; set; }
+    }
+}
---
+++ RetailersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GreatOutdoors.Mvc.Models;
+using GreatOutdoors.Entities;
+using Capgemini.GreatOutdoors.BusinessLayer;
+using System.Threading.Tasks;
+using static System.ValueTuple;
+
+namespace GreatOutdoors.Mvc.Controllers
+{
+    public class RetailersController : Controller
+    {
+        // URL: Retailers/Create
+        public async Task<ActionResult> Create()
+        {
+            //Creating and initailizing viewamodel object
+            RetailerViewModel retailerViewModel = new RetailerViewModel()
+            {
+
+                // RetailerName = "Sarthak",
+                // RetailerMobile = "7023511335",
+                // ModifiedDateTime = DateTime.Now
+
+            };
+            //ceating object oftype RetailerBL
+            RetailerBL retailerBL = new RetailerBL();
+
+            //List of retailers from RetailerBL
+            List<Retailer> retailers = await retailerBL.GetAllRetailersBL();
+            //
+
+
+            //calling view 
+            return View(retailerViewModel);
+        }
+
+        // URL: Retailers/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(RetailerViewModel retailerVM)
+        {
+            //create object of personBL
+
+            RetailerBL retailerBL = new RetailerBL();
+
+            //create object of retailer Entity Model
+            Retailer retailer = new Retailer();
+
+            //storing fields in retailer(Entity)  object form retailerVM object
+            retailer.RetailerName = retailerVM.RetailerName;
+            retailer.RetailerMobile = retailerVM.RetailerMobile;
+            retailer.Email = retailerVM.Email;
+            // retailer.Gender = retailerVM.Gender;
+            retailer.RetailerPassword = retailerVM.RetailerPassword;
+
+
+            //call the AddretailerBL method to add retailer
+            (bool IsAdded, Guid RetailerID) = await retailerBL.AddRetailerBL(retailer);
+            if (IsAdded)
+            {
+                return Content("This route will register the retailer");
+            }
+            else
+            {
+                return Content("Retailer not registered");
+            }
+        }
+    }
+}
